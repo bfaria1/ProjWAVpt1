75ab209fab6bdfe9d80b13d65321533607aca78e
bfaria1 <bia99faria1@gmail.com>
2021-02-10 14:59:46 -0300

    Ancova - Rascunho

--- ANCOVA.R
+++ ANCOVA.R
@@ -7,6 +7,8 @@ library("openxlsx")
 
 dados <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/R/ProjWAV/Excel/S7/ancova.xlsx", rowNames = TRUE)
 
+# Teste se os fatores externos têm influência quanto a quantidade de espécies (N=173)
+
 A <- aov(Altitude ~ WAVE, data = dados)
 summary(A)
 
@@ -20,5 +22,20 @@ A <- aov(Latitude ~ WAVE, data = dados)
 summary(A)
 
 A <- aov(Longitude ~ WAVE, data = dados)
+summary(A)
+
+A <- aov(Altitude ~ SLIE, data = dados)
+summary(A)
+
+A <- aov(Área ~ SLIE, data = dados)
+summary(A)
+
+A <- aov(População ~ SLIE, data = dados)
+summary(A)
+
+A <- aov(Latitude ~ SLIE, data = dados)
+summary(A)
+
+A <- aov(Longitude ~ SLIE, data = dados)
 summary(A)
 
