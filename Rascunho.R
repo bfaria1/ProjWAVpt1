c5123d21db5cab51966e91af05587dbbae1ea62c
bfaria1 <bia99faria1@gmail.com>
2021-02-25 20:48:35 -0300

    Fevereiro, 2021

---
+++ Rascunho.R
@@ -0,0 +1,316 @@
+library("openxlsx") 
+library(ggplot2)
+library(RColorBrewer)
+library(dplyr)
+library(ggpubr)
+library(tidyr)
+library(hrbrthemes)
+library(sf)
+library(geobr)
+library(factoextra)
+library(ade4)
+library(vegan)
+library(gclus)
+library(cluster)
+library(FD)
+library(ggcorrplot)
+library(rstatix)
+library(car)
+library(emmeans)
+library(multcomp)
+library(usethis)
+library(moments)
+# Seção 1
+
+WS <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/R/S1/WAVxSPL.xlsx") 
+WAV <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/R/S1/WAV.xlsx") 
+SPL <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/R/S1/SPL.xlsx")
+ESP <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/R/S1/Espécies.xlsx")
+dados <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/R/S1/dados.xlsx")
+
+A1 <- hist(WAV$WAVR, 
+           plot = FALSE, 
+           breaks = c(0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2,3.4,3.6,3.8,4.0,4.2,4.4,4.6,4.8,5.0))
+A1$density = A1$counts/sum(A1$counts)*100
+
+B1 <- hist(SPL$SPLR, 
+           plot = FALSE, 
+           breaks = c(0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2,3.4,3.6,3.8,4.0,4.2,4.4,4.6,4.8,5.0))
+B1$density = B1$counts/sum(B1$counts)*100
+
+C1 <- hist (WS$WAV2R, 
+            plot = FALSE, 
+            breaks = c(0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2,3.4,3.6,3.8,4.0,4.2,4.4,4.6,4.8,5.0))
+C1$density = C1$counts/sum(C1$counts)*100
+
+A2 <- hist(WAV$WAVE, 
+           plot = FALSE, 
+           breaks = c(0,0.12,0.24,0.36,0.48,0.60,0.72,0.84,0.96,1.08,1.20,1.32,1.44,1.56,1.68,1.80,1.92,2.04,2.16,2.28,2.40,2.52,2.64,2.76,2.88,3.00)) 
+A2$density = A2$counts/sum(A2$counts)*100
+
+B2 <- hist(SPL$SPLE, 
+           plot = FALSE, 
+           breaks = c(0,0.12,0.24,0.36,0.48,0.60,0.72,0.84,0.96,1.08,1.20,1.32,1.44,1.56,1.68,1.80,1.92,2.04,2.16,2.28,2.40,2.52,2.64,2.76,2.88,3.00))
+B2$density = B2$counts/sum(B2$counts)*100
+
+C2 <- hist(WS$WAV2E, 
+           plot = FALSE,
+           breaks = c(0,0.12,0.24,0.36,0.48,0.60,0.72,0.84,0.96,1.08,1.20,1.32,1.44,1.56,1.68,1.80,1.92,2.04,2.16,2.28,2.40,2.52,2.64,2.76,2.88,3.00)) 
+C2$density = C2$counts/sum(C2$counts)*100
+
+A3 <- hist(ESP$WAV, 
+           plot = FALSE, 
+           breaks = c(0,0.16,0.32,0.48,0.64,0.8,0.96,1.12,1.28,1.44,1.60,1.76,1.92,2.08,2.24,2.40,2.56,2.72,2.88,3.04,3.20,3.36,3.52,3.68,3.84,4.00))
+A3$density = A3$counts/sum(A3$counts)*100
+
+B3 <- hist(ESP$SPL, 
+           plot = FALSE, 
+           breaks = c(0,0.16,0.32,0.48,0.64,0.8,0.96,1.12,1.28,1.44,1.60,1.76,1.92,2.08,2.24,2.40,2.56,2.72,2.88,3.04,3.20,3.36,3.52,3.68,3.84,4.00))
+B3$density = B3$counts/sum(B3$counts)*100
+
+C3 <- hist(ESP$WAV2, 
+           plot = FALSE,
+           breaks = c(0,0.16,0.32,0.48,0.64,0.8,0.96,1.12,1.28,1.44,1.60,1.76,1.92,2.08,2.24,2.40,2.56,2.72,2.88,3.04,3.20,3.36,3.52,3.68,3.84,4.00))
+C3$density = C3$counts/sum(C3$counts)*100
+
+y1 <- A1$density
+x1 <- c()
+x1[1] <- 0.1
+for (i in 1:24){
+  x1[i+1] = x1[i]+0.2
+}
+
+
+y2 <- B1$density
+x2 <- c()
+x2[1] <- 0.1
+for (i in 1:24){
+  x2[i+1] = x2[i]+0.2
+}
+
+
+y3 <- C1$density
+x3 <- c()
+x3[1] <- 0.1
+for (i in 1:24){
+  x3[i+1] = x3[i]+0.2
+}
+
+AX <- c()
+
+for (i in 1:25){
+  AX[i] = "WAV (N = 631)"
+}
+for (i in 26:50){
+  AX[i] = "SLI (N = 174)"
+}
+for (i in 51:75){
+  AX[i] = "WAV 2 (N = 173)"
+}
+
+BX <- c()
+
+for (i in 1:25){
+  BX[i] = x1[i]
+}
+
+for(i in 26:50){
+  BX[i] = x2[i-25]
+}
+for(i  in 51:75){
+  BX[i] = x3[i-50]
+}
+
+CX<- c()
+
+for (i in 1:25){
+  CX[i] = y1[i]
+}
+
+for(i in 26:50){
+  CX[i] = y2[i-25]
+}
+for(i  in 51:75){
+  CX[i] = y3[i-50]
+}
+
+dados1 <- data.frame(L = AX, x = BX, y = CX)
+dados1$L <- factor(dados1$L, levels = c("WAV (N = 631)", "SLI (N = 174)", "WAV 2 (N = 173)"))
+
+G1<- ggplot(dados1, aes(x = x, y = CX, group = L, colour = L))+
+  geom_line()+
+  scale_colour_manual(values = c("blue", "red", "green")) +
+  labs(x = "Número de Registros (Log10)",
+       y = "Frequência Relativa (%)",
+       colour = " ")+
+  theme_classic()
+
+
+
+y1 <- A2$density
+x1 <- c()
+x1[1] <- 0.1
+for (i in 1:24){
+  x1[i+1] = x1[i]+0.12
+}
+
+y2 <- B2$density
+x2 <- c()
+x2[1] <- 0.1
+for (i in 1:24){
+  x2[i+1] = x2[i]+0.12
+}
+
+y3 <- C2$density
+x3 <- c()
+x3[1] <- 0.1
+for (i in 1:24){
+  x3[i+1] = x3[i]+0.12
+}
+
+AY <- c()
+
+for (i in 1:25){
+  AY[i] = "WAV (N = 631)"
+}
+for (i in 26:50){
+  AY[i] = "SLI (N = 174)"
+}
+for (i in 51:75){
+  AY[i] = "WAV 2 (N = 173)"
+}
+
+BY <- c()
+
+for (i in 1:25){
+  BY[i] = x1[i]
+}
+
+for(i in 26:50){
+  BY[i] = x2[i-25]
+}
+for(i  in 51:75){
+  BY[i] = x3[i-50]
+}
+
+CY<- c()
+
+for (i in 1:25){
+  CY[i] = y1[i]
+}
+
+for(i in 26:50){
+  CY[i] = y2[i-25]
+}
+for(i  in 51:75){
+  CY[i] = y3[i-50]
+}
+
+dados2 <- data.frame(L = AY, x = BY, y = CY)
+dados2$L <- factor(dados2$L, levels = c("WAV (N = 631)", "SLI (N = 174)", "WAV 2 (N = 173)"))
+
+G2 <- ggplot(dados2, aes(x = x, y = CY, group = L, colour = L))+
+  geom_line()+
+  scale_colour_manual(values = c("blue", "red", "green")) +
+  labs(x = "Número de Espécies (Log10)",
+       y = "Frequência Relativa (%)",
+       colour = " ")+
+  theme_classic()
+
+
+
+
+y1 <- A3$density
+x1 <- c()
+x1[1] <- 0.1
+for (i in 1:24){
+  x1[i+1] = x1[i]+0.16
+}
+
+y2 <- B3$density
+x2 <- c()
+x2[1] <- 0.1
+for (i in 1:24){
+  x2[i+1] = x2[i]+0.16
+}
+
+y3 <- C3$density
+x3 <- c()
+x3[1] <- 0.1
+for (i in 1:24){
+  x3[i+1] = x3[i]+0.16
+}
+
+AZ <- c()
+
+for (i in 1:25){
+  AZ[i] = "WAV (S = 790)"
+}
+for (i in 26:50){
+  AZ[i] = "SLI (S = 661)"
+}
+for (i in 51:75){
+  AZ[i] = "WAV 2 (S = 779)"
+}
+
+BZ <- c()
+
+A <- c()
+
+for (i in 1:25){
+  A[i] = "WAV (S = 790)"
+}
+for (i in 26:50){
+  A[i] = "SLI (S = 661)"
+}
+for (i in 51:75){
+  A[i] = "WAV 2 (S = 779)"
+}for (i in 1:25){
+  BZ[i] = x1[i]
+}
+
+for(i in 26:50){
+  BZ[i] = x2[i-25]
+}
+for(i  in 51:75){
+  BZ[i] = x3[i-50]
+}
+
+CZ<- c()
+
+for (i in 1:25){
+  CZ[i] = y1[i]
+}
+
+for(i in 26:50){
+  CZ[i] = y2[i-25]
+}
+for(i  in 51:75){
+  CZ[i] = y3[i-50]
+}
+
+dados3 <- data.frame(L = AZ, x = BZ, y = CZ)
+dados3$L <- factor(dados3$L, levels = c("WAV (S = 790)", "SLI (S = 661)", "WAV 2 (S = 779)"))
+
+G3<- ggplot(dados3, aes(x = x, y = CZ, group = L, colour = L))+
+  geom_line()+
+  scale_colour_manual(values = c("blue", "red", "green")) +
+  labs(x = "Número de Registros por Espécie (Log10)",
+       y = "Frequência Relativa (%)",
+       colour = " ")+
+  theme_classic()
+
+text <- paste("
+              
+              
+              Figura 1: Distribuição de municípios em classes segundo o número de registros (Log10), em cada banco de dados: WAV = Wikiaves, SLI = SpeciesLink, WAV2 = WAV com municípios redundantes em SLI. n = número de municípios.",
+              sep = " ")
+
+Texto <- ggparagraph(text, size = 12)
+
+#png("S1.png", width = 550, height = 530)
+ggarrange(G1,G2,G3,Texto, ncol = 2 , nrow = 2, legend = "top")
+#dev.off()
+
+#rm(list=ls())
+
