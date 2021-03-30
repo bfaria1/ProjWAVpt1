e38388e5946fde69c2a0fb68de62cb1cd1be4e5c
Beatriz de Faria <bd.faria1@gmail.com>
2021-03-30 13:20:29 -0300

    Março, 2021

--- Seção 6.R
+++ Seção 6.R
@@ -10,6 +10,8 @@ library(dendextend)
 library(ade4)
 library(vegan)
 
+# Todos os Municípios
+
 WAV <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/WAV-E.xlsx", sheet = "Geral")
 
 WAV <- t(WAV)
@@ -23,6 +25,8 @@ is.numeric(SLI)
 dados <- read.xlsx("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Mapas.xlsx")
 mc <- read_municipality(code_muni= "SP")
 
+maps <- merge(mc,dados)
+
 A <- vegdist(WAV, method = "jaccard", binary = TRUE)
 B <- vegdist(SLI, method = "jaccard", binary = TRUE)
 
@@ -34,12 +38,6 @@ fviz_gap_stat(gap_stat1)
 gap_stat2 <- clusGap(SLI, FUN = hcut, K.max = 25, B = 10)
 fviz_gap_stat(gap_stat2)
 
-plot(hc1, cex = 0.6, hang = -1)
-rect.hclust(hc1, k = 6, border = 2:5)
-
-plot(hc2, cex = 0.6, hang = -1)
-rect.hclust(hc2, k = 6, border = 2:5)
-
 dend1 <- as.dendrogram (hc1)
 dend2 <- as.dendrogram (hc2)
 
@@ -48,6 +46,161 @@ entanglement(dend1,
              L = 1,
              leaves_matching_method = c("order"))
 
+## K = 2
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 2)
+
+
+grp1 <- cutree(hc1, k = 2)
+grp1
+
+grp2 <- cutree(hc2, k = 2)
+grp2
+
+maps$WAV2 <- cut(maps$dendWAV2, breaks = c(-2,0,1,2),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2"))
+
+maps$SLI2 <- cut(maps$dendSLI2, breaks = c(-2,0,1,2),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2"))
+
+ggplot(maps) +
+  geom_sf(aes(fill=WAV2), color = NA) +
+  labs(subtitle="Wikiaves", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+ggplot(maps) +
+  geom_sf(aes(fill=SLI2), color = NA) +
+  labs(subtitle="SpeciesLink", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+
+
+## K = 3
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 3)
+
+
+grp1 <- cutree(hc1, k = 3)
+grp1
+
+grp2 <- cutree(hc2, k = 3)
+grp2
+
+maps$WAV3 <- cut(maps$dendWAV3, breaks = c(-2,0,1,2,3),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3"))
+
+maps$SLI3 <- cut(maps$dendSLI3, breaks = c(-2,0,1,2,3),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3"))
+
+ggplot(maps) +
+  geom_sf(aes(fill=WAV3), color = NA) +
+  labs(subtitle="Wikiaves", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+ggplot(maps) +
+  geom_sf(aes(fill=SLI3), color = NA) +
+  labs(subtitle="SpeciesLink", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+## K = 4
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 4)
+
+
+grp1 <- cutree(hc1, k = 4)
+grp1
+
+grp2 <- cutree(hc2, k = 4)
+grp2
+
+maps$WAV4 <- cut(maps$dendWAV4, breaks = c(-2,0,1,2,3,4),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3","Grupo 4"))
+
+maps$SLI4 <- cut(maps$dendSLI4, breaks = c(-2,0,1,2,3,4),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3","Grupo 4"))
+
+ggplot(maps) +
+  geom_sf(aes(fill=WAV4), color = NA) +
+  labs(subtitle="Wikiaves", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+ggplot(maps) +
+  geom_sf(aes(fill=SLI4), color = NA) +
+  labs(subtitle="SpeciesLink", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+## K = 5
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 5)
+
+
+grp1 <- cutree(hc1, k = 5)
+grp112
+
+grp2 <- cutree(hc2, k = 5)
+grp2
+
+maps$WAV5 <- cut(maps$dendWAV5, breaks = c(-2,0,1,2,3,4,5),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3","Grupo 4","Grupo 5"))
+
+maps$SLI5 <- cut(maps$dendSLI5, breaks = c(-2,0,1,2,3,4,5),
+                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3","Grupo 4","Grupo 5"))
+
+ggplot(maps) +
+  geom_sf(aes(fill=WAV5), color = NA) +
+  labs(subtitle="Wikiaves", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+ggplot(maps) +
+  geom_sf(aes(fill=SLI5), color = NA) +
+  labs(subtitle="SpeciesLink", size=8)+ 
+  scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
+  labs(fill = " ")+
+  theme_minimal()
+
+## K = 6
 
 tanglegram(dend1, dend2,
            highlight_distinct_edges = FALSE, 
@@ -67,27 +220,85 @@ grp2 <- cutree(hc2, k = 6)
 grp2
 
 
-maps <- merge(mc,dados)
-
-maps$WAV <- cut(maps$dendWAV, breaks = c(-2,0,1,2,3,4,5,6),
+maps$WAV6 <- cut(maps$dendWAV6, breaks = c(-2,0,1,2,3,4,5,6),
               labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3","Grupo 4","Grupo 5","Grupo 6"))
 
-maps$SLI <- cut(maps$dendSLI, breaks = c(-2,0,1,2,3,4,5,6),
+maps$SLI6 <- cut(maps$dendSLI6, breaks = c(-2,0,1,2,3,4,5,6),
                 labels = c("Sem Registro","Grupo 1","Grupo 2","Grupo 3","Grupo 4","Grupo 5","Grupo 6"))
 
 ggplot(maps) +
-  geom_sf(aes(fill=WAV), color = NA) +
+  geom_sf(aes(fill=WAV6), color = NA) +
   labs(subtitle="Wikiaves", size=8)+ 
   scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
   labs(fill = " ")+
   theme_minimal()
 
 ggplot(maps) +
-  geom_sf(aes(fill=SLI), color = NA) +
+  geom_sf(aes(fill=SLI6), color = NA) +
   labs(subtitle="SpeciesLink", size=8)+ 
   scale_fill_manual (values = c('#f0f0f0','#B646C7','#228B00','#9F7000','#0082CE','#CC476B','#009681'))+
   labs(fill = " ")+
   theme_minimal()
+
+## K = 7
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 7)
+
+grp1 <- cutree(hc1, k = 7)
+grp1
+
+grp2 <- cutree(hc2, k = 7)
+grp2
+
+## K = 8
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 8)
+
+
+grp1 <- cutree(hc1, k = 8)
+grp1
+
+grp2 <- cutree(hc2, k = 8)
+grp2
+
+## K = 9
+
+tanglegram(dend1, dend2,
+           highlight_distinct_edges = FALSE, 
+           common_subtrees_color_lines = TRUE, 
+           common_subtrees_color_branches = FALSE,
+           main_left = "Wikiaves",
+           main_right = "SpeciesLink",
+           lwd = .5,
+           k_branches = 9)
+
+
+grp1 <- cutree(hc1, k = 9)
+grp1
+
+grp2 <- cutree(hc2, k = 9)
+grp2
+
+
+# Top 50
+
+# Top 20
+
+# Top 10
 
 
 rm(list = ls())
