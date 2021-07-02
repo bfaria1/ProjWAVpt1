d425d39e3c783658f5e1f8627a2e32e5d85fed3b
Beatriz de Faria <bd.faria1@gmail.com>
2021-07-02 15:06:25 -0300

    Julho, 2021

--- Seção 6.R
+++ Seção 6.R
@@ -25,15 +25,19 @@ SLI2 <- t(SLI2)
 is.numeric(SLI2)
 
 dados <- read.xlsx("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Mapas.xlsx")
+dados2 <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos2.xlsx", sheet = "Mapas")
 mc <- read_municipality(code_muni= "SP")
 
 maps <- merge(mc,dados)
+maps2 <- merge(mc, dados2)
 
 N1 <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos.xlsx", sheet = "K = 2")
 N2 <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos.xlsx", sheet = "K = 3")
 N3 <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos.xlsx", sheet = "K = 4")
 N4 <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos.xlsx", sheet = "K = 5")
 N5 <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos.xlsx", sheet = "K = 6")
+
+N <- read.xlsx ("C:/Users/bia99/OneDrive/Documents/Wikiaves/ProjWAV/Excel/S6/Grupos2.xlsx", sheet = "Dados")
 
 A <- vegdist(WAV, method = "jaccard", binary = TRUE)
 B <- vegdist(SLI, method = "jaccard", binary = TRUE)
@@ -737,6 +741,66 @@ grp1
 grp2 <- cutree(hc4, k = 2)
 grp2
 
+byf.shapiro(AL ~ GW2, N)
+byf.shapiro(AR ~ GW2, N)
+byf.shapiro(POP ~ GW2, N)
+byf.shapiro(LA ~ GW2, N)
+byf.shapiro(LO ~ GW2, N)
+
+byf.shapiro(AL ~ GS2, N)
+byf.shapiro(AR ~ GS2, N)
+byf.shapiro(POP ~ GS2, N)
+byf.shapiro(LA ~ GS2, N)
+byf.shapiro(LO ~ GS2, N)
+
+
+leveneTest(AL ~ GW2, N, mean)
+leveneTest(AR ~ GW2, N, mean)
+leveneTest(POP ~ GW2, N, mean)
+leveneTest(LA ~ GW2, N, mean)
+leveneTest(LO ~ GW2, N, mean)
+
+leveneTest(AL ~ GS2, N, mean)
+leveneTest(AR ~ GS2, N, mean)
+leveneTest(POP ~ GS2, N, mean)
+leveneTest(LA ~ GS2, N, mean)
+leveneTest(LO ~ GS2, N, mean)
+
+oneway.test(AL ~ GW2, data = N, var.equal=FALSE)
+oneway.test(AR ~ GW2, data = N, var.equal=FALSE)
+oneway.test(POP ~ GW2, data = N, var.equal=FALSE)
+oneway.test(LA ~ GW2, data = N, var.equal=FALSE)
+oneway.test(LO ~ GW2, data = N, var.equal=FALSE)
+
+oneway.test(AL ~ GS2, data = N, var.equal=FALSE)
+oneway.test(AR ~ GS2, data = N, var.equal=TRUE)
+oneway.test(POP ~ GS2, data = N, var.equal=TRUE)
+oneway.test(LA ~ GS2, data = N, var.equal=FALSE)
+oneway.test(LO ~ GS2, data = N, var.equal=TRUE)
+
+
+mod <- aov(AL ~ GW2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GW2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GW2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GW2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GW2, N)
+PostHocTest(mod, method = "hsd")
+
+mod <- aov(AL ~ GS2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GS2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GS2, N5)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GS2, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GS2, N)
+PostHocTest(mod, method = "hsd")
+
 
 # k = 3
 
@@ -755,6 +819,67 @@ grp1
 
 grp2 <- cutree(hc4, k = 3)
 grp2
+
+
+byf.shapiro(AL ~ GW3, N)
+byf.shapiro(AR ~ GW3, N)
+byf.shapiro(POP ~ GW3, N)
+byf.shapiro(LA ~ GW3, N)
+byf.shapiro(LO ~ GW3, N)
+
+byf.shapiro(AL ~ GS3, N)
+byf.shapiro(AR ~ GS3, N)
+byf.shapiro(POP ~ GS3, N)
+byf.shapiro(LA ~ GS3, N)
+byf.shapiro(LO ~ GS3, N)
+
+
+leveneTest(AL ~ GW3, N, mean)
+leveneTest(AR ~ GW3, N, mean)
+leveneTest(POP ~ GW3, N, mean)
+leveneTest(LA ~ GW3, N, mean)
+leveneTest(LO ~ GW3, N, mean)
+
+leveneTest(AL ~ GS3, N, mean)
+leveneTest(AR ~ GS3, N, mean)
+leveneTest(POP ~ GS3, N, mean)
+leveneTest(LA ~ GS3, N, mean)
+leveneTest(LO ~ GS3, N, mean)
+
+oneway.test(AL ~ GW3, data = N, var.equal=FALSE)
+oneway.test(AR ~ GW3, data = N, var.equal=FALSE)
+oneway.test(POP ~ GW3, data = N, var.equal=FALSE)
+oneway.test(LA ~ GW3, data = N, var.equal=FALSE)
+oneway.test(LO ~ GW3, data = N, var.equal=FALSE)
+
+oneway.test(AL ~ GS3, data = N, var.equal=FALSE)
+oneway.test(AR ~ GS3, data = N, var.equal=TRUE)
+oneway.test(POP ~ GS3, data = N, var.equal=TRUE)
+oneway.test(LA ~ GS3, data = N, var.equal=FALSE)
+oneway.test(LO ~ GS3, data = N, var.equal=TRUE)
+
+
+mod <- aov(AL ~ GW3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GW3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GW3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GW3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GW3, N)
+PostHocTest(mod, method = "hsd")
+
+mod <- aov(AL ~ GS3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GS3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GS3, N5)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GS3, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GS3, N)
+PostHocTest(mod, method = "hsd")
 
 # k = 4
 
@@ -775,6 +900,66 @@ grp1
 grp2 <- cutree(hc4, k = 4)
 grp2
 
+byf.shapiro(AL ~ GW4, N)
+byf.shapiro(AR ~ GW4, N)
+byf.shapiro(POP ~ GW4, N)
+byf.shapiro(LA ~ GW4, N)
+byf.shapiro(LO ~ GW4, N)
+
+byf.shapiro(AL ~ GS4, N)
+byf.shapiro(AR ~ GS4, N)
+byf.shapiro(POP ~ GS4, N)
+byf.shapiro(LA ~ GS4, N)
+byf.shapiro(LO ~ GS4, N)
+
+
+leveneTest(AL ~ GW4, N, mean)
+leveneTest(AR ~ GW4, N, mean)
+leveneTest(POP ~ GW4, N, mean)
+leveneTest(LA ~ GW4, N, mean)
+leveneTest(LO ~ GW4, N, mean)
+
+leveneTest(AL ~ GS4, N, mean)
+leveneTest(AR ~ GS4, N, mean)
+leveneTest(POP ~ GS4, N, mean)
+leveneTest(LA ~ GS4, N, mean)
+leveneTest(LO ~ GS4, N, mean)
+
+oneway.test(AL ~ GW4, data = N, var.equal=FALSE)
+oneway.test(AR ~ GW4, data = N, var.equal=FALSE)
+oneway.test(POP ~ GW4, data = N, var.equal=FALSE)
+oneway.test(LA ~ GW4, data = N, var.equal=FALSE)
+oneway.test(LO ~ GW4, data = N, var.equal=FALSE)
+
+oneway.test(AL ~ GS4, data = N, var.equal=FALSE)
+oneway.test(AR ~ GS4, data = N, var.equal=TRUE)
+oneway.test(POP ~ GS4, data = N, var.equal=TRUE)
+oneway.test(LA ~ GS4, data = N, var.equal=FALSE)
+oneway.test(LO ~ GS4, data = N, var.equal=TRUE)
+
+
+mod <- aov(AL ~ GW4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GW4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GW4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GW4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GW4, N)
+PostHocTest(mod, method = "hsd")
+
+mod <- aov(AL ~ GS4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GS4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GS4, N5)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GS4, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GS4, N)
+PostHocTest(mod, method = "hsd")
+
 # k = 5
 
 tanglegram(dend3, dend4,
@@ -793,6 +978,65 @@ grp1
 grp2 <- cutree(hc4, k = 5)
 grp2
 
+byf.shapiro(AL ~ GW5, N)
+byf.shapiro(AR ~ GW5, N)
+byf.shapiro(POP ~ GW5, N)
+byf.shapiro(LA ~ GW5, N)
+byf.shapiro(LO ~ GW5, N)
+
+byf.shapiro(AL ~ GS5, N)
+byf.shapiro(AR ~ GS5, N)
+byf.shapiro(POP ~ GS5, N)
+byf.shapiro(LA ~ GS5, N)
+byf.shapiro(LO ~ GS5, N)
+
+
+leveneTest(AL ~ GW5, N, mean)
+leveneTest(AR ~ GW5, N, mean)
+leveneTest(POP ~ GW5, N, mean)
+leveneTest(LA ~ GW5, N, mean)
+leveneTest(LO ~ GW5, N, mean)
+
+leveneTest(AL ~ GS5, N, mean)
+leveneTest(AR ~ GS5, N, mean)
+leveneTest(POP ~ GS5, N, mean)
+leveneTest(LA ~ GS5, N, mean)
+leveneTest(LO ~ GS5, N, mean)
+
+oneway.test(AL ~ GW5, data = N, var.equal=FALSE)
+oneway.test(AR ~ GW5, data = N, var.equal=FALSE)
+oneway.test(POP ~ GW5, data = N, var.equal=FALSE)
+oneway.test(LA ~ GW5, data = N, var.equal=FALSE)
+oneway.test(LO ~ GW5, data = N, var.equal=FALSE)
+
+oneway.test(AL ~ GS5, data = N, var.equal=FALSE)
+oneway.test(AR ~ GS5, data = N, var.equal=TRUE)
+oneway.test(POP ~ GS5, data = N, var.equal=TRUE)
+oneway.test(LA ~ GS5, data = N, var.equal=FALSE)
+oneway.test(LO ~ GS5, data = N, var.equal=TRUE)
+
+
+mod <- aov(AL ~ GW5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GW5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GW5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GW5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GW5, N)
+PostHocTest(mod, method = "hsd")
+
+mod <- aov(AL ~ GS5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(AR ~ GS5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(POP ~ GS5, N5)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LA ~ GS5, N)
+PostHocTest(mod, method = "hsd")
+mod <- aov(LO ~ GS5, N)
+PostHocTest(mod, method = "hsd")
 
 
 rm(list = ls())
